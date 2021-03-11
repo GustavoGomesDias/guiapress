353a81b53323d89b5dc13232b19bbb464cde193b
GustavoGomesDias <treeelevengg@gmail.com>
2021-03-11 01:17:19 -0300

    Acertando timezone do sequelize e salbando artigos - Aula 94

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require('slugify');
 
-router.get('/articles', (req, res) => {
+router.get('/admin/articles', (req, res) => {
     res.send("Rota de articles")
 });
 
@@ -13,5 +15,21 @@ router.get("/admin/articles/new", (req, res) => {
         });
     })
 });
+
+router.post("/articles/save", (req, res) => {
+    const title = req.body.title;
+    const body = req.body.body;
+    const category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect('/admin/articles');
+    });
+
+})
 
 module.exports = router;
