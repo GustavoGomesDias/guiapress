488e9e7ae5d16e0312980d72bb18b34893150c94
GustavoGomesDias <treeelevengg@gmail.com>
2021-03-13 23:13:48 -0300

    Aula 104 e 105 - Dividindo os artigos de cada Categoria e exibindo elas.

--- index.js
+++ index.js
@@ -39,9 +39,14 @@ app.get("/", (req, res) => {
             ['id', 'DESC']
         ]
     }).then(articles => {
-        res.render("index", {
-            articles: articles
+
+        Category.findAll().then(categories => {
+            res.render("index", {
+                articles: articles,
+                categories: categories
+            });
         });
+
     });
 });
 
@@ -53,9 +58,13 @@ app.get('/:slug', (req, res) => {
         }
     }).then(article => {
         if(article != undefined){
-            res.render("article", {
-                article: article
+            Category.findAll().then(categories => {
+                res.render("article", {
+                    article: article,
+                    categories: categories
+                });
             });
+    
         }else{
             res.redirect("/");
         }
@@ -63,6 +72,32 @@ app.get('/:slug', (req, res) => {
         console.log(err);
         res.redirect("/")
     })
+});
+
+app.get("/category/:slug", (req, res) => {
+    const slug = req.params.slug;
+    console.log(slug);
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then(category => {
+        if(category != undefined){
+        
+            Category.findAll().then(categories => {
+                res.render("index", {
+                    articles: category.articles,
+                    categories: categories
+                })
+            });
+
+        }else{
+            res.redirect("/");
+        }
+    }).catch(err => {
+        console.log(err)
+    });
 });
 
 app.listen(8080, () => {
