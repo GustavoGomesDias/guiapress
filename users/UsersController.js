e2d3d4b6b1849b7576387ae7791f3c8ef2ca77e5
GustavoGomesDias <treeelevengg@gmail.com>
2021-03-15 18:59:01 -0300

    Aula 117 - Formulário de criação de usuário

--- users/UsersController.js
+++ users/UsersController.js
@@ -10,5 +10,12 @@ router.get("/admin/users/create", (req, res) => {
     res.render("admin/users/create");
 });
 
+router.post("/users/create", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    res.json({email, password});
+});
+
 
 module.exports = router;
