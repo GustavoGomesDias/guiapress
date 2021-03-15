e342b03429b818ffa83c0af194ac56d20e3e6b01
GustavoGomesDias <treeelevengg@gmail.com>
2021-03-15 18:37:05 -0300

    Aula 116 - Criação do controller e do model de users

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const connection = require('./database/database');
 // Controllers
 const categoriesController = require('./categories/CategoriesController');
 const articlesController = require('./articles/articlesController');
+const usersController = require('./users/UsersController');
 
 // Models
 const Article = require('./articles/Article');
 const Category = require('./categories/Category');
+const User = require('./users/User');
 
 // View engine
 app.set('view engine', 'ejs');
@@ -32,6 +34,7 @@ connection
 
 app.use('/', categoriesController);
 app.use('/', articlesController);
+app.use('/', usersController);
 
 app.get("/", (req, res) => {
     Article.findAll({
